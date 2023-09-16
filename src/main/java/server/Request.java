d8b59c0093aaa0f463c841276d42af90ad9a379a
DmitriiMurin <murin.dima@gmail.com>
2023-09-16 14:32:58 +0300

    [NF] query params parsing

--- src/main/java/server/Request.java
+++ src/main/java/server/Request.java
@@ -1,5 +1,10 @@
 package server;
 
+import org.apache.commons.collections.map.MultiValueMap;
+import org.apache.http.NameValuePair;
+
+import java.util.List;
+
 public class Request {
 
     private static final String DEFAULT_HTTP_VERSION = "HTTP/1.1";
@@ -7,12 +12,20 @@ public class Request {
 
     private String addressLine;
 
+    private MultiValueMap paramsMap = new MultiValueMap();
     private String httpVersion;
 
     public Request(Method method, String addressLine, String httpVersion) {
         this.method = method;
         this.addressLine = addressLine;
         this.httpVersion = httpVersion;
+    }
+
+    public Request(Method method, String addressLine, String httpVersion, List<NameValuePair> paramsList) {
+        this.method = method;
+        this.addressLine = addressLine;
+        this.httpVersion = httpVersion;
+        paramsList.forEach((item) -> paramsMap.put(item.getName(), item.getValue()));
     }
 
     public Request(Method method, String addressLine) {
@@ -25,7 +38,15 @@ public class Request {
         return addressLine;
     }
 
-    public RequestKey getRequestKey(){
+    public RequestKey getRequestKey() {
         return new RequestKey(method, addressLine);
+    }
+
+    public Object getQueryParam(String name) {
+        return paramsMap.get(name);
+    }
+
+    public MultiValueMap getQueryParams() {
+        return paramsMap;
     }
 }
